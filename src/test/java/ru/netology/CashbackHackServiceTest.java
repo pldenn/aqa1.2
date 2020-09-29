b9af31856e6306ab025060695947d52dde529ccd
Denis Timofeev <y719xy@gmail.com>
2020-09-29 15:40:23 +0300

    test ci should be fail

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Assertions;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-//    @Test
-//    public void shouldReturnNull() {
-//        int actual = service.remain(1000);
-//        int expected = 0;
-//        Assert.assertEquals(expected, actual);
-//        //TODO: You need to fix that         28.09.2020
-//    }
+    @Test
+    public void shouldReturnNull() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+        //TODO: You need to fix that         28.09.2020
+    }
 
     @Test
     public void shouldReturn200() {
